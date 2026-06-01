72a6423ecea0f431cd8c6a00d896247f56ab837a
segzyswitch <segzyswitch99@gmail.com>
2026-06-01 15:52:10 +0100

    static logo urlmaker

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -107,6 +107,18 @@ const getCardStyle = (image: string) => {
 
 const loadReq = ref(false);
 
+const dataUrlToFile = (dataUrl: string, filename: string): File => {
+  const arr = dataUrl.split(',')
+  const mime = arr[0].match(/:(.*?);/)?.[1] || 'image/jpeg'
+  const bstr = atob(arr[1])
+  let n = bstr.length
+  const u8arr = new Uint8Array(n)
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n)
+  }
+  return new File([u8arr], filename, { type: mime })
+}
+
 const submitForm = () => {
   if ( frontPreview.value === '' || backPreview.value === '' ) {
     $swal.fire('Error', 'Please upload both front and back images of your ID.', 'error');
